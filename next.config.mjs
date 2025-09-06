0a6f63db0432f21c5303c25aa63c7f157332ba2b
Orbin-Ahmed <93181928+Orbin-Ahmed@users.noreply.github.com>
2025-09-06 00:51:56 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,45 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// import createNextIntlPlugin from "next-intl/plugin";
+
+// const nextConfig = {
+//   output: "standalone",
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "**",
+//       },
+//     ],
+//   },
+//   experimental: {},
+// };
+
+// const withNextIntl = createNextIntlPlugin();
+
+// export default withNextIntl(nextConfig);
+
+import path from "path";
 import createNextIntlPlugin from "next-intl/plugin";
-
-const nextConfig = {
-  output: "standalone",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
-  experimental: {},
-};
-
 const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
+  },
+
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname),
+  },
+
+  outputFileTracingIgnores: [
+    {
+      "**/*": ["**/.next/**", "**/.turbo/**", "**/.cache/**", "**/.vercel/**"],
+    },
+    { "**/*": ["**/dist/**", "**/build/**"] },
+    { "**/*": ["**/.git/**", "**/.idea/**"] },
+    { "**/*": ["**/*.map", "**/*.log"] },
+  ],
+};
 
 export default withNextIntl(nextConfig);
