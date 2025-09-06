ada9aa28e0f880e44cc597bf7238cf87b20ec928
Orbin-Ahmed <93181928+Orbin-Ahmed@users.noreply.github.com>
2025-09-06 00:53:33 -0400

    modified Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// import createNextIntlPlugin from "next-intl/plugin";
-
-// const nextConfig = {
-//   output: "standalone",
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "**",
-//       },
-//     ],
-//   },
-//   experimental: {},
-// };
-
-// const withNextIntl = createNextIntlPlugin();
-
-// export default withNextIntl(nextConfig);
+/** @type {import('next').NextConfig} */
 
 import path from "path";
 import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "**" }],
---
+++ types/backupConfig.ts
@@ -0,0 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// import createNextIntlPlugin from "next-intl/plugin";
+
+// const nextConfig = {
+//   output: "standalone",
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "**",
+//       },
+//     ],
+//   },
+//   experimental: {},
+// };
+
+// const withNextIntl = createNextIntlPlugin();
+
+// export default withNextIntl(nextConfig);
